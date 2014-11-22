9118e3833edaa66e0cf2cd3f5a4d05a343d9cbf3
Chanwit Kaewkasi <chanwit@gmail.com>
2014-11-22 19:34:39 +0700

    added tab indentation

--- diff_gen.groovy
+++ diff_gen.groovy
@@ -1,12 +1,19 @@
-lines = new File("demo.diff").readLines()
+
+void tab() {
+    print("\t")
+}
+
+lines = new File("demo/demo.diff").readLines()
 def hunk = lines.findIndexOf { it.startsWith("@@") }
 def (info, context) = lines[hunk].split("@@").collect { it.trim() }[1..2]
 
+println("patch")
+
 info = info.replaceAll(/[\-\+]/,'').split(/,| /).collect { Integer.valueOf(it) }
-println("find\t\"$context\"")
+tab(); println("find\t\"$context\"")
 3.times { t ->
     def text = lines[hunk + t +1].replaceAll(/\t/,"\\\\t")
-    println("next\t\"${text}\"")
+    tab(); println("next\t\"${text}\"")
 }
 println()
 i = hunk + 4
@@ -14,18 +21,22 @@ def mark = lines[i][0]
 while(mark == '-' || mark == '+') {
     def text = lines[i].substring(1).replaceAll(/\t/,"\\\\t")
     if(mark == "-") {
-        println "delete\t\"${text}\""
+        tab(); println "delete\t\"${text}\""
     } else if(mark == "+"){
-        println "add\t\"${text}\""
+        tab(); println "add\t\"${text}\""
     }
     i++
     mark = lines[i][0]
 }
+println()
 3.times { t ->
     if(lines[i + t].length() <= 1) {
-        println("next\t\"\"")
+        tab(); println("next\t\"\"")
     } else {
         def text = lines[i + t].substring(1).replaceAll(/\t/,"\\\\t")
-        println("next\t\"${text}\"")
+        tab(); println("next\t\"${text}\"")
     }
 }
+
+tab(); println("commit")
+println("done")
