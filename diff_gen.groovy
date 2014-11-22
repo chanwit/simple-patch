e56bb6c57fbdfa138363154d5a9a1b6e6068bb47
Chanwit Kaewkasi <chanwit@gmail.com>
2014-11-22 20:27:53 +0700

    still caused some error when generate a complex diff

--- diff_gen.groovy
+++ diff_gen.groovy
@@ -3,39 +3,81 @@ void tab() {
     print("\t")
 }
 
-lines = new File("demo/demo.diff").readLines()
-def hunk = lines.findIndexOf { it.startsWith("@@") }
-def (info, context) = lines[hunk].split("@@").collect { it.trim() }[1..2]
+lines = new File("demo/0001-added-tab-indentation.patch").readLines()
 
-println("patch")
+def subject = lines.findIndexOf { it.startsWith("Subject:") }
+def name = lines[subject] - "Subject: [PATCH] "
+println("name\t\"$name\"")
 
-info = info.replaceAll(/[\-\+]/,'').split(/,| /).collect { Integer.valueOf(it) }
-tab(); println("find\t\"$context\"")
-3.times { t ->
-    def text = lines[hunk + t +1].replaceAll(/\t/,"\\\\t")
-    tab(); println("next\t\"${text}\"")
-}
-println()
-i = hunk + 4
-def mark = lines[i][0]
-while(mark == '-' || mark == '+') {
-    def text = lines[i].substring(1).replaceAll(/\t/,"\\\\t")
-    if(mark == "-") {
-        tab(); println "delete\t\"${text}\""
-    } else if(mark == "+"){
-        tab(); println "add\t\"${text}\""
+def diffCmd = lines.findIndexOf { it.startsWith("diff --git") }
+def cmd = lines[diffCmd].split()
+def filename = cmd[2] - "a/"
+
+println("patch\t\"$filename\"")
+
+def i = 0
+
+while(true) {
+    def hunk = lines.findIndexOf(i){ it.startsWith("@@") }
+    if(hunk == -1) {
+        // all hunk processed
+        break
     }
-    i++
-    mark = lines[i][0]
-}
-println()
-3.times { t ->
-    if(lines[i + t].length() <= 1) {
-        tab(); println("next\t\"\"")
+
+    def hunkData = lines[hunk].split("@@")
+    def info = hunkData[1].trim()
+    def context = hunkData.size() == 3 ? hunkData[2].trim() : ""
+
+    //println ">> DEBUG: ${info}"
+
+    info = info.replaceAll(/[\-\+]/,'').split(/,| /).collect { Integer.valueOf(it) }
+    if(context) {
+        tab(); println("find\t\"$context\"")
     } else {
-        def text = lines[i + t].substring(1).replaceAll(/\t/,"\\\\t")
-        tab(); println("next\t\"${text}\"")
+        tab()
+        println "goto\t1"
     }
+
+    i = hunk + 1
+    def mark = lines[i][0]
+    def find = true
+
+    while(i < lines.size()) {
+
+        while(mark == '-' || mark == '+') {
+            def text = lines[i].substring(1).replaceAll(/\t/,"\\\\t")
+            if(mark == "-") {
+                tab(); println "delete\t\"${text}\""
+            } else if(mark == "+"){
+                tab(); println "add\t\"${text}\""
+            }
+            i++; if(i >= lines.size()) break
+            mark = lines[i][0]
+        }
+        println()
+
+        while(mark != '-' && mark != '+') {
+            def text = lines[i].substring(1).replaceAll(/\t/,"\\\\t")
+            if(find) {
+                tab(); println("find\t\"$text\"")
+                find = false
+            } else {
+                tab(); println("next\t\"$text\"")
+            }
+            i++; if(i >= lines.size()) break
+            mark = lines[i][0]
+            if(mark == "@") {
+                break;
+            }
+        }
+        println()
+        if(mark == "@") {
+            break;
+        }
+
+        if(i >= lines.size()) break
+    }
+
 }
 
 tab(); println("commit")
